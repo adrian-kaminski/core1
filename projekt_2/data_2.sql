01b223aa93bfcd10d8f67537ca3898b647e815f8
adrian-kaminski <58092127+adrian-kaminski@users.noreply.github.com>
2023-08-14 17:14:51 +0200

    Update data_2.sql

--- projekt_2/data_2.sql
+++ projekt_2/data_2.sql
@@ -3,3 +3,15 @@ create temporary table tmp_table as select * from tmp.tmp;
 select top 100 * from tmp_table;
 
 create table tmp2_table as select * from tmp_table;
+
+select 1 from tmp;
+select 1 from tmp;
+select 1 from tmp;
+select 1 from tmp;
+select 1 from tmp;
+select 1 from tmp;
+select 1 from tmp;
+select 1 from tmp;
+select 1 from tmp;
+
+
