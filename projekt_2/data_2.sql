7b723853a615f287b5e6fa94f789939b4fb08197
adrian-kaminski <58092127+adrian-kaminski@users.noreply.github.com>
2023-08-14 17:28:07 +0200

    Update data_2.sql delete rows

--- projekt_2/data_2.sql
+++ projekt_2/data_2.sql
@@ -5,13 +5,4 @@ select top 100 * from tmp_table;
 create table tmp2_table as select * from tmp_table;
 
 select 1 from tmp;
-select 1 from tmp;
-select 1 from tmp;
-select 1 from tmp;
-select 1 from tmp;
-select 1 from tmp;
-select 1 from tmp;
-select 1 from tmp;
-select 1 from tmp;
-
 
